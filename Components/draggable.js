372fbef6cf47a925a4375303c12592bdfd29cde6
Roman Velnar <roman.velnar@spikeglobal.com>
2023-01-04 13:59:26 +0000

    bugfix and added react-reanimate

--- Components/draggable.js
+++ Components/draggable.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useCallback, useMemo, useRef } from "react";
 import { View, Text } from "react-native"
+import BottomSheet from "@gorhom/bottom-sheet";
 
 export function Draggable() {
+    const bottomSheetRef = useRef<BottomSheet>(null);
+    const snapPoints = useMemo(() => ['25%', '50%'], []);
+    const handleSheetChanges = useCallback((index) => {
+        console.log('handleSheetChanges', index)
+    }, [])
+
     return(
-        <View style={{height: "100%", backgroundColor: "blue", }}>
-            <Text>Some more stuff...</Text>
+        <View style={{flex: 1, backgroundColor: "blue"}}>
+            <BottomSheet 
+                ref={bottomSheetRef}
+                index={1}
+                snapPoints={snapPoints}
+                onChange={handleSheetChanges}
+            />
+            <View>
+                <Text>Blablabalblab</Text>
+                <Text>Blablabalblab</Text>
+                <Text>Blablabalblab</Text>
+                <Text>Blablabalblab</Text>
+                <Text>Blablabalblab</Text>
+                <Text>Blablabalblab</Text>
+                <Text>Blablabalblab</Text>
+                <Text>Blablabalblab</Text>
+            </View>    
+
         </View>
     )
 }
