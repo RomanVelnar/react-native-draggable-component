0b0b62533bc3d9effc2b45faa87a2b5f11f0bd95
Roman Velnar <roman.velnar@spikeglobal.com>
2023-01-05 10:31:10 +0000

    setted up the environment and created the components

---
+++ components/ContentComponent.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export function ContentComponent() {
+    return(
+        <View>
+            <Text>fndasofndsonfasnd</Text>
+            <Text>fndasofndsonfasnd</Text>
+            <Text>fndasofndsonfasnd</Text>
+            <Text>fndasofndsonfasnd</Text>
+            <Text>fndasofndsonfasnd</Text>
+            <Text>fndasofndsonfasnd</Text>
+            <Text>fndasofndsonfasnd</Text>
+            <Text>fndasofndsonfasnd</Text>
+        </View>
+    )
+}
