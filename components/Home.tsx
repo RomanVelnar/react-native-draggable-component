ef64d4d0a4cb4e760f88cf9f2f3d32a81a691c53
Roman Velnar <roman.velnar@spikeglobal.com>
2023-01-09 13:38:34 +0000

    bugfix + code cleanup

--- components/Home.tsx
+++ components/Home.tsx
@@ -6,19 +6,21 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export function Home() {
   return (
-    <GestureHandlerRootView style={{flex: 1}}>
-        <View style={{ flex: 1, backgroundColor: "gray" }}>
-            <TouchableOpacity onPress={() => console.log('dfadfaf')}><Text>dafdfasfasf</Text></TouchableOpacity>
-            <Draggable>
-            <ContentComponent style={{ height: 60, marginBottom: 10 }} />
-            <View style={{ flexDirection: "row", marginBottom: 10 }}>
-                <ContentComponent
-                style={{ flex: 1, height: 120, marginRight: 10 }}
-                />
-                <ContentComponent style={{ flex: 1, height: 120 }} />
-            </View>
-            </Draggable>
-        </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: "gray" }}>
+        <TouchableOpacity onPress={() => console.log("dfadfaf")}>
+          <Text>dafdfasfasf</Text>
+        </TouchableOpacity>
+        <Draggable minHeight={} >
+          <ContentComponent style={{ height: 60, marginBottom: 10 }} />
+          <View style={{ flexDirection: "row", marginBottom: 10 }}>
+            <ContentComponent
+              style={{ flex: 1, height: 120, marginRight: 10 }}
+            />
+            <ContentComponent style={{ flex: 1, height: 120 }} />
+          </View>
+        </Draggable>
+      </View>
     </GestureHandlerRootView>
   );
 }
